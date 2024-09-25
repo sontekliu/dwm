6db04e2aff675d97aecf2206b9c1e736377718c0
liu_sj <sontek@yeah.net>
2024-09-25 23:42:21 +0800

    add viewontag

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=14",
 					"WenQuanYi Micro Hei:size=14:style=Regular:antialias=true:auto:autohint=true",
 					"Symbols Nerd Font:pixelsize=16:style=Regular:antialias=true:auto:autohint=true"
--- dwm.c
+++ dwm.c
@@ -1947,6 +1947,8 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+		if(viewontag && ((arg->ui & TAGMASK) != TAGMASK))
+			view(arg);
 	}
 }
 
