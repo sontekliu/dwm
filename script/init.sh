8becebdd82be96d619269c1f6b97182de9793e5a
liu_sj <sontek@yeah.net>
2024-09-27 23:20:29 +0800

    add scripts

---
+++ script/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "start"
+
+# /usr/bin/rofi -show drun --show-icon &
+
+while true; do 
+	feh --bg-fill --randomize /home/laoliu/myconfig/bg
+	sleep 1m
+done
---
+++ script/init_blocking.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "start"
+
+/usr/bin/rofi -show drun --show-icon &
